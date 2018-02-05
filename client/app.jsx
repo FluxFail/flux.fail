ace243205e8e484964bfb389fadbe046463a275b
Henri Bergius <henri.bergius@iki.fi>
2018-02-05 13:16:24 +0100

    More verbose color definitions

--- client/app.jsx
+++ client/app.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FluxFail from './elements/FluxFail';
-import { yellow600 } from 'material-ui/styles/colors'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 const fluxfailTheme = getMuiTheme({
   palette: {
-    primary1Color: yellow600,
+    primary1Color: '#fdd835',
+    primary2Color: '#ffff6b',
+    primary3Color: '#c6a700',
+    pickerHeaderColor: '#fdd835',
+    accent1Color: '#78909c',
+    accent2Color: '#a7c0cd',
+    accent3Color: '#4b636e',
+    textColor: '#000000',
+    alternateTextColor: '#000000',
   },
 });
 
