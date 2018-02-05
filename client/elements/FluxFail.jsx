3948da0e4ffd74802e21288fcfe1ff6f3d9b0c23
Henri Bergius <henri.bergius@iki.fi>
2018-02-05 12:46:50 +0100

    Plug in the delay form

--- client/elements/FluxFail.jsx
+++ client/elements/FluxFail.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import DelayForm from './DelayForm';
+
+const appBarStyle = {
+  position: 'fixed',
+  top: '0px',
+};
+
+const fabStyle = {
+  position: 'fixed',
+  right: '42px',
+  bottom: '42px',
+};
 
 const FluxFail = () => (
-  <AppBar
+  <div>
+    <AppBar
       title="Flux.Fail"
-  />
+      style={appBarStyle}
+    />
+    <main>
+      <DelayForm />
+    </main>
+    <FloatingActionButton style={fabStyle}>
+      <ContentAdd />
+    </FloatingActionButton>
+  </div>
 );
 
 export default FluxFail;
