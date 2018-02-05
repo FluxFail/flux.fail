7f7ff576ef8e3f66f5a946e011bd8464902a211d
Henri Bergius <henri.bergius@iki.fi>
2018-02-05 13:05:42 +0100

    Go Berlin yellow

--- client/app.jsx
+++ client/app.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FluxFail from './elements/FluxFail';
+import { yellow600 } from 'material-ui/styles/colors'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const fluxfailTheme = getMuiTheme({
+  palette: {
+    primary1Color: yellow600,
+  },
+});
 
 const App = () => (
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={fluxfailTheme}>
       <FluxFail />
     </MuiThemeProvider>
 );
