df841b1662ac1deed3f169086efe997684102531
Henri Bergius <henri.bergius@iki.fi>
2018-02-16 19:19:21 +0100

    User and token grant tables

--- server/migrations/20180115190143_initial.js
+++ server/migrations/20180115190143_initial.js
@@ -7,23 +7,49 @@
  *   - delay in minutes
  *   - total travel delay caused by this line delay
  */
-exports.up = knex => knex.schema.createTable('delay', (t) => {
-  // Time information
-  t.timestamps();
-  // Transport type
-  t.string('type').index();
-  // Country (ISO 3166-1 alpha-2)
-  t.string('country', 2).index();
-  // City
-  t.string('city').index();
-  // Line
-  t.string('line').index();
-  // End station / direction
-  t.string('direction');
-  // Delay in minutes
-  t.integer('delay');
-  // Total delay caused by this segment
-  t.integer('total_delay');
-});
+exports.up = (knex) => {
+  knex.schema.createTable('user', (t) => {
+    t.uuid('id').primary();
+    t.string('email').index();
+    t.timestamps();
+  })
+    .then(() => {
+      knex.schema.createTable('auth', (t) => {
+        t.uuid('user').notNullable().references('id').inTable('user');
+        t.string('grant').index();
+        t.timestamps();
+      });
+    })
+    .then(() => {
+      knex.schema.createTable('delay', (t) => {
+        t.uuid('id').primary();
+        t.uuid('user').notNullable().references('id').inTable('user');
+        // Time information
+        t.timestamps();
+        // Transport type
+        t.string('type').index();
+        // Country (ISO 3166-1 alpha-2)
+        t.string('country', 2).index();
+        // City
+        t.string('city').index();
+        // Line
+        t.string('line').index();
+        // End station / direction
+        t.string('direction');
+        // Delay in minutes
+        t.integer('delay');
+        // Total delay caused by this segment
+        t.integer('total_delay');
+      });
+    });
+};
 
-exports.down = knex => knex.schema.dropTable('delay');
+exports.down = (knex) => {
+  knex.schema.dropTable('delay')
+    .then(() => {
+      knex.schema.dropTable('auth');
+    })
+    .then(() => {
+      knex.schema.dropTable('user');
+    });
+};
