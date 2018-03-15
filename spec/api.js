609704956b2a5f7c6310c8467116ff72a9dfc6a2
Henri Bergius <henri.bergius@iki.fi>
2018-03-15 19:21:07 +0100

    Test login flow

--- spec/api.js
+++ spec/api.js
@@ -2,8 +2,10 @@ const request = require('supertest');
 const { expect } = require('chai');
 
 const api = request('http://127.0.0.1:8080');
+const email = request('http://127.0.0.1:1080');
 
 describe('FluxFail API', () => {
+  let apiToken = null;
   it('should be available', () => {
     return api
       .get('/')
@@ -32,6 +34,62 @@ describe('FluxFail API', () => {
           .then((res) => {
             expect(res.body.message).to.contain('Invalid email');
           })
+      });
+    });
+    describe('with valid email address', () => {
+      let grantToken = null;
+      after(() => {
+        // Clear received emails
+        return email
+          .delete('/api/emails')
+      });
+      it('should return 202', () => {
+        return api
+          .post('/login/email')
+          .send({
+            email: 'user@example.com',
+          })
+          .expect(202);
+      });
+      it('should have sent an email with grant token', () => {
+        return email
+          .get('/api/emails')
+          .expect(200)
+          .then((res) => {
+            expect(res.body.length).to.equal(1);
+            const matched = res.body[0].text.match(/\?token=([a-zA-Z0-9]+)/);
+            expect(matched).to.be.an('array');
+            grantToken = matched[1];
+            expect(grantToken).to.be.a('string');
+          });
+      });
+      it('should enable converting grant token to API token', () => {
+        return api
+          .post('/login/exchange')
+          .send({
+            token: grantToken,
+          })
+          .expect(200)
+          .then((res) => {
+            expect(res.body.token).to.be.a('string');
+            apiToken = res.body.token;
+          });
+      });
+    });
+  });
+  describe('Delay reporting', () => {
+    before(() => {
+      expect(apiToken).to.be.a('string');
+    });
+    describe('initially', () => {
+      it('should return an empty list of delays', () => {
+        return api
+          .get('/delay')
+          .set('Authorization', `Bearer ${apiToken}`)
+          .expect(200)
+          .then((res) => {
+            expect(res.body).eql([]);
+          });
       });
     });
   });
