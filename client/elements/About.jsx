be7063717dbbf678d0378c5335709790da182157
Brian Wiborg <baccenfutter@c-base.org>
2018-11-15 02:27:58 +0100

    public beta pre-launch

--- client/elements/About.jsx
+++ client/elements/About.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const About = () => (
   <div>
     <h1>About Flux.Fail</h1>
-    <p>Here goes basic description, privacy policy, impressum.</p>
+    <center>
+      <strong>flux.fail wird betrieben von:</strong><br />
+      <hr />
+      <pre>
+        flux.fail Holding UG (haftungsbeschränkt)<br />
+        Glogauer Straße 21<br />
+        10999 Berlin
+      </pre>
+      Eingetragen beim Amtsgericht Berlin-Charlottenburg unter HRB200262 B.
+      <hr />
+      Inhaltlich verantwortlich: Brian Wiborg
+    </center>
   </div>
 );
 
