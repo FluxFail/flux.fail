325170efdc0bf1d0f5d60ac8d1b5ad5a5488e7a8
Brian Wiborg <baccenfutter@c-base.org>
2019-01-30 15:42:49 +0100

    initialize VueJS client

--- client/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = store => next => (action) => {
-  console.log('dispatching', action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  return result;
-};
-
-export default logger;
